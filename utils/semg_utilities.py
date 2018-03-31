11c2d4d959d455274a232dcca56e3d2afaacf16d
Daniel Burer <dwburer@gmail.com>
2018-03-31 03:34:44 -0400

    6 classes

--- utils/semg_utilities.py
+++ utils/semg_utilities.py
@@ -10,25 +10,21 @@ def read_data(data_path, split = "train"):
     """ Read data """
 
     # Fixed params
-    n_class = 2
+    n_class = 6
+    n_channels = 2
     n_steps = 2500
-
-    # Paths
-    # path_ = os.path.join(data_path, split)
-    # path_signals = os.path.join(path_, "Inertial_Signals")
-
-    # # Read labels and one-hot encode
-    # label_path = os.path.join(path_, "y_" + split + ".txt")
-    # labels = pd.read_csv(label_path, header = None)
 
     # hardcode for two types right now
     # first 100 are cyl, second 100 are hook
     # cyl = 1
     # hook = 2
+    # lat = 3
+    # palm = 4
+    # spher = 5
+    # tip = 6
     labels = np.concatenate(
         (
-            [1 for i in range(100)],
-            [2 for i in range(100)]
+            [[class_id for _ in range(100)] for class_id in range(1, n_class + 1)]
         )
     )
 
@@ -38,48 +34,38 @@ def read_data(data_path, split = "train"):
     # n_channels = len(channel_files)
     # posix = len(split) + 5
 
-
-    channel_files = [
-        data_path + 'cyl_ch1.csv',
-        data_path + 'cyl_ch2.csv',
-        data_path + 'hook_ch1.csv',
-        data_path + 'hook_ch2.csv'
+    files = [
+        'cyl_ch1.csv',
+        'cyl_ch2.csv',
+        'hook_ch1.csv',
+        'hook_ch2.csv',
+        'lat_ch1.csv',
+        'lat_ch2.csv',
+        'palm_ch1.csv',
+        'palm_ch2.csv',
+        'spher_ch1.csv',
+        'spher_ch2.csv',
+        'tip_ch1.csv',
+        'tip_ch2.csv',
     ]
-    n_channels = 2
+
+    channel_files = [data_path + filename for filename in files]
 
     # merge files of different grip types into one long file, per channel
-    all_channel_1 = (
-        pd.concat([pd.read_csv(channel_files[0],  header=None), pd.read_csv(channel_files[2],  header=None)]),
-        'channel_1'
-    )
+    channels = []
+    for num_channel in range(n_channels):
+        all_of_channel = [pd.read_csv(channel_files[file * (num_channel * n_channels)],  header=None) for file in range(int(len(channel_files) / n_channels))]
 
-    all_channel_2 = (
-        pd.concat([pd.read_csv(channel_files[1],  header=None), pd.read_csv(channel_files[3],  header=None)]),
-        'channel_2'
-    )
+        channels.append(
+            (pd.concat(all_of_channel), 'channel_%d' % num_channel)
+        )
 
     # Initiate array
     list_of_channels = []
     X = np.zeros((len(labels), n_steps, n_channels))
 
-
-
-    # i_ch = 0
-    # for fil_ch in channel_files:
-    #     channel_name = fil_ch[:-posix]
-    #     dat_ = pd.read_csv(os.path.join(path_signals,fil_ch), delim_whitespace = True, header = None)
-    #     X[:,:,i_ch] = dat_.as_matrix()
-
-    #     # Record names
-    #     print(channel_name)
-    #     list_of_channels.append(channel_name)
-
-    #     # iterate
-    #     i_ch += 1
-
-
     i_ch = 0
-    for channel_data, channel_name in [all_channel_1, all_channel_2]:
+    for channel_data, channel_name in channels:
         X[:,:,i_ch] = channel_data.as_matrix()
         list_of_channels.append(channel_name)
 
@@ -98,7 +84,7 @@ def standardize(train, test):
 
     return X_train, X_test
 
-def one_hot(labels, n_class = 2):
+def one_hot(labels, n_class = 6):
     """ One-hot encoding """
     expansion = np.eye(n_class)
     y = expansion[:, labels-1].T
